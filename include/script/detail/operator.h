d36f8caa92a7a08ca751fa48ce96d437c800bc16
Neroll <1343809614way@gmail.com>
2024-09-26 21:24:28 +0800

    添加加减乘除取模运算的仿函数类

---
+++ include/script/detail/operator.h
@@ -0,0 +1,57 @@
+#ifndef NEROLL_SCRIPT_DETAIL_OPERATOR_H
+#define NEROLL_SCRIPT_DETAIL_OPERATOR_H
+
+namespace neroll {
+
+namespace script {
+
+namespace detail {
+
+class plus {
+ public:
+    template <typename T, typename U>
+    constexpr auto operator()(T &lhs, U &rhs) {
+        return lhs + rhs;
+    }
+};
+
+class minus {
+ public:
+    template <typename T, typename U>
+    constexpr auto operator()(T &lhs, U &rhs) {
+        return lhs - rhs;
+    }
+};
+
+class multiplies {
+ public:
+    template <typename T, typename U>
+    constexpr auto operator()(T &lhs, U &rhs) {
+        return lhs * rhs;
+    }
+};
+
+
+class divides {
+ public:
+    template <typename T, typename U>
+    constexpr auto operator()(T &lhs, U &rhs) {
+        return lhs / rhs;
+    }
+};
+
+class modulus {
+ public:
+    template <typename T, typename U>
+    constexpr auto operator()(T &lhs, U &rhs) {
+        return lhs % rhs;
+    }
+};
+
+}   // namespace detail
+
+}   // namespace script
+
+}   // namespace neroll
+
+#endif
