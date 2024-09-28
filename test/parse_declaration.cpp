de2fad15a72914d53b9e0e6ce6368672ba7c9f73
Neroll <1343809614way@gmail.com>
2024-09-28 21:57:07 +0800

    完善了 parser 类的 parse_declaration 方法的测试

--- test/parse_declaration.cpp
+++ test/parse_declaration.cpp
@@ -14,17 +14,25 @@ int main() {
     nsd::parser<nsd::input_stream_adapter> parser{nsd::lexer<nsd::input_stream_adapter>{nsd::input_stream_adapter{fin}}};
 
     try {
+        std::string var_name = "a";
         nsd::statement_node *node = parser.parse_declaration();
         node->execute();
 
-        std::println("{}", nsd::program_scope.contains("neroll"));
+        std::println("{}", nsd::program_scope.contains(var_name));
 
-        auto var = nsd::program_scope.find("neroll");
+        auto var = nsd::program_scope.find(var_name);
+        assert(var != nullptr);
 
         std::println("variable name: {}", var->name());
         std::println("variable type: {}", nsd::variable_type_name(var->type()));
-        assert(var->type() == nsd::variable_type::integer);
-        std::println("variable value: {}", std::dynamic_pointer_cast<nsd::variable_int>(var)->value());
+
+        if (var->type() == nsd::variable_type::integer) {
+            std::println("variable value: {}", std::dynamic_pointer_cast<nsd::variable_int>(var)->value());
+        } else if (var->type() == nsd::variable_type::floating) {
+            std::println("variable value: {}", std::dynamic_pointer_cast<nsd::variable_float>(var)->value());
+        } else if (var->type() == nsd::variable_type::boolean) {
+            std::println("variable value: {}", std::dynamic_pointer_cast<nsd::variable_boolean>(var)->value());
+        }
     }
     catch (std::exception &e) {
         std::println("{}", e.what());
